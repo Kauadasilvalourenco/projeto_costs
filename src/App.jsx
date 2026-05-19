c50b39c7331e77f5d407a1364a14c3e291c9b512
Kauã da Silva Lourenço <122580902+Kauadasilvalourenco@users.noreply.github.com>
2026-05-19 20:19:29 -0300

    feat: Adicionado Rotas e gerenciamento de rotas no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return(
-    <div>
-      Página principal
+    <div className="App">
+
+      <main className="conteiner_principal">
+
+        <Routes>
+
+          <Route 
+            path="/"
+          />
+          
+          <Route 
+            path="/projetos"
+          />
+
+          <Route 
+            path="/contato"
+          />
+
+          <Route 
+            path="/empresa"
+          />
+
+        </Routes>
+
+      </main>
+
     </div>
   )
 }
