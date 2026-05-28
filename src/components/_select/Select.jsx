60539e955b0502316016c6df82da9f214506a5fb
Kauã da Silva Lourenço <122580902+Kauadasilvalourenco@users.noreply.github.com>
2026-05-27 21:46:13 -0300

    feat: Criado estrutura e estilo componente Select

---
+++ src/components/_select/Select.jsx
@@ -0,0 +1,26 @@
+import styleSelect from "./Select.module.css";
+// import css;
+
+function Select({name, id, options, onChange, style}) {
+    return(
+        <div>
+            <select 
+                name={name}
+                id={id}
+                onChange={onChange}
+                className={`${styleSelect.select} ${style}`}
+                required
+            >
+                <option hidden>Escolha uma Categoria</option>
+
+                {options.map((option) => (
+                    <option key={option.id} value={option.nome} className={styleSelect.option}>
+                        {option.nome}
+                    </option>
+                ))}
+            </select>
+        </div>
+    )
+}
+
+export default Select;
--- src/pages/criar-projeto/CriarProjeto.jsx
+++ src/pages/criar-projeto/CriarProjeto.jsx
@@ -4,9 +4,17 @@ import { useId } from "react";
 import Typography from "../../components/_typography/Typography";
 import Label from "../../components/_label/Label";
 import Input from "../../components/_input/Input";
+import Select from "../../components/_select/Select";
 // import components;
 
 function CriarProjeto() {
+    const categorias = [
+        {id: 1, nome: "Infra"},
+        {id: 2, nome: "Planejamento"},
+        {id: 3, nome: "Desenvolvimento"},
+        {id: 4, nome: "Design"}
+    ];
+
     const ID = useId();
 
     return(
@@ -25,6 +33,7 @@ function CriarProjeto() {
             </Typography>
 
             <form>
+
                 <div className="conteiner_nome_projeto">
                     <Label
                         htmlFor={`${ID}-nome_projeto`}
@@ -38,6 +47,34 @@ function CriarProjeto() {
                       placeholder={"Ex: Criação de Landing Page"}
                     />
                 </div>
+
+                <div className="conteiner_orcamento_projeto">
+                    <Label
+                        htmlFor={`${ID}-orcamento_projeto`}
+                    >
+                        Orçamento do Projeto:
+                    </Label>
+                    <Input 
+                      type={"number"}
+                      name={`${ID}-orcamento_projeto`}
+                      id={`${ID}-orcamento_projeto`}
+                      placeholder={"Ex: R$5000,00"}
+                    />
+                </div>
+
+                <div className="conteiner_categorias_projeto">
+                    <Label
+                        htmlFor={`${ID}-categorias_projeto`}
+                    >
+                        Selecione a Categoria:
+                    </Label>
+                    <Select 
+                        name={`${ID}-categorias_projeto`}
+                        id={`${ID}-categorias_projeto`}
+                        options={categorias}
+                    />
+                </div>
+
             </form>
 
         </div>
