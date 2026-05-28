2960eff9b8e39e518bbd14c971c090cad22368e5
Kauã da Silva Lourenço <122580902+Kauadasilvalourenco@users.noreply.github.com>
2026-05-27 21:03:22 -0300

    feat: Criado estrutura e estilo do componente Label Adicionado configuração de cor_label no :root

--- src/pages/criar-projeto/CriarProjeto.jsx
+++ src/pages/criar-projeto/CriarProjeto.jsx
@@ -1,7 +1,36 @@
+import { useId } from "react";
+// import react;
+
+import Typography from "../../components/_typography/Typography";
+import Label from "../../components/_label/Label";
+// import components;
+
 function CriarProjeto() {
+    const ID = useId();
+
     return(
         <div>
-            Página Criação Projetos
+
+            <Typography
+                tag={"h1"}
+            >
+                Criar Projeto
+            </Typography>
+
+            <Typography
+                tag={"p"}
+            >
+                Crie seu projeto para depois adicionar os serviços!
+            </Typography>
+
+            <form>
+                <Label 
+                    htmlFor={`${ID}-nome_projeto`}
+                >
+                    Nome do Projeto:
+                </Label>
+            </form>
+
         </div>
     )
 }
