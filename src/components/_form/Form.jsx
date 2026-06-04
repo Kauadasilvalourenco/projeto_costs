13ce65a96d5b76b569aaa820ad98a7d9e7742b45
Kauã da Silva Lourenço <122580902+Kauadasilvalourenco@users.noreply.github.com>
2026-06-03 22:26:22 -0300

    style: Criado estilo do Form.jsx

--- src/components/_form/Form.jsx
+++ src/components/_form/Form.jsx
@@ -1,11 +1,13 @@
 import { useId } from "react";
 // import ID;
 
+import { formSchema } from "./_schema/formSchema";
+// import Schema;
+
 import Button from "../_button/Button";
 // import components;
 
-import { formSchema } from "./_schema/formSchema";
-// import Schema;
+import styleForm from "./Form.module.css";
 
 function Form({fieldsConfig, btnText}) {
     const ID = useId();
@@ -17,14 +19,14 @@ function Form({fieldsConfig, btnText}) {
 
     return(
         <div>
-            <form onSubmit={submit}>
+            <form onSubmit={submit} className={styleForm.Form}>
                 {
                     fieldsList.map((item, index) => {
                         const TagLabel = item.label.component;
                         const TagField = item.field.component;
 
                         return (
-                            <div key={index}>
+                            <div key={index} className={styleForm.conteiner_fields}>
                                 <TagLabel 
                                     {...item.label.props}
                                 />
@@ -37,7 +39,8 @@ function Form({fieldsConfig, btnText}) {
                 }
 
                 <Button
-                   type={"submit"} 
+                   type={"submit"}
+                   style={styleForm.button}
                 >
                     {btnText}
                 </Button>
--- src/pages/criar-projeto/CriarProjeto.jsx
+++ src/pages/criar-projeto/CriarProjeto.jsx
@@ -1,29 +1,67 @@
-import { useId } from "react";
-// import react;
-
 import Typography from "../../components/_typography/Typography";
-import Label from "../../components/_label/Label";
 import Input from "../../components/_input/Input";
 import Select from "../../components/_select/Select";
-import Button from "../../components/_button/Button";
+import Form from "../../components/_form/Form";
 // import components;
 
 import styleCriarProjeto from "./CriarProjeto.module.css";
 // import css;
 
 function CriarProjeto() {
-    const categorias = [
+    const categorys = [
         {id: 1, nome: "Infra"},
         {id: 2, nome: "Planejamento"},
         {id: 3, nome: "Desenvolvimento"},
         {id: 4, nome: "Design"}
     ];
 
-    const ID = useId();
-
-    function criarProjeto(e) {
-        e.preventDefault();
-    }
+    const projectForm = [
+        {
+            label: {
+                props: {
+                    children: "Nome do Projeto: "
+                }
+            },
+            field: {
+                component: {
+                    type: Input
+                },
+                props: {
+                    name: "nome_projeto", type: "text", placeholder: "EX: Criação Landing Page"
+                }
+            }
+        },
+        {
+            label: {
+                props: {
+                    children: "Orçamento do Projeto: "
+                }
+            },
+            field: {
+                component: {
+                    type: Input
+                },
+                props: {
+                    name: "orcamento_projeto", type: "number", placeholder: "EX: R$5000,00"
+                }
+            }
+        },
+        {
+            label: {
+                props: {
+                    children: "Selecione uma Categoria: "
+                }
+            },
+            field: {
+                component: {
+                    type: Select
+                },
+                props: {
+                    name: "categoria_projeto", options: categorys
+                }
+            }
+        }
+    ]
 
     return(
         <div className={styleCriarProjeto.CriarProjeto}>
@@ -40,56 +78,10 @@ function CriarProjeto() {
                 Crie seu projeto para depois adicionar os serviços!
             </Typography>
 
-            <form>
-
-                <div className={styleCriarProjeto.conteiner_nome_projeto}>
-                    <Label
-                        htmlFor={`${ID}-nome_projeto`}
-                    >
-                        Nome do Projeto:
-                    </Label>
-                    <Input 
-                      type={"text"}
-                      name={`${ID}-nome_projeto`}
-                      id={`${ID}-nome_projeto`}
-                      placeholder={"Ex: Criação de Landing Page"}
-                    />
-                </div>
-
-                <div className={styleCriarProjeto.conteiner_orcamento_projeto}>
-                    <Label
-                        htmlFor={`${ID}-orcamento_projeto`}
-                    >
-                        Orçamento do Projeto:
-                    </Label>
-                    <Input 
-                      type={"number"}
-                      name={`${ID}-orcamento_projeto`}
-                      id={`${ID}-orcamento_projeto`}
-                      placeholder={"Ex: R$5000,00"}
-                    />
-                </div>
-
-                <div className={styleCriarProjeto.conteiner_categorias_projeto}>
-                    <Label
-                        htmlFor={`${ID}-categorias_projeto`}
-                    >
-                        Selecione a Categoria:
-                    </Label>
-                    <Select 
-                        name={`${ID}-categorias_projeto`}
-                        id={`${ID}-categorias_projeto`}
-                        options={categorias}
-                    />
-                </div>
-
-                <Button
-                    type={"submit"}
-                    onClick={criarProjeto}
-                    children={"Criar Projeto"}
-                />
-
-            </form>
+            <Form 
+                fieldsConfig={projectForm}
+                btnText={"Criar Projeto"}
+            />
 
         </div>
     )
