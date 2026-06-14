bfefd251f16dafe920d013abc5a08b0313d21170
Kauã da Silva Lourenço <122580902+Kauadasilvalourenco@users.noreply.github.com>
2026-06-14 20:21:39 -0300

    feat: Adicionado teste de comportamente unitário ao componente Input.jsx

--- src/components/_input/Input.test.jsx
+++ src/components/_input/Input.test.jsx
@@ -1,5 +1,6 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Input from "./Input";
 
 describe("Componente Input", () => {
@@ -18,5 +19,21 @@ describe("Componente Input", () => {
         expect(inputElement).toHaveAttribute("type", "text");
         expect(inputElement).toHaveAttribute("name", "input_component");
         expect(inputElement).toHaveAttribute("placeholder", "Digite Aqui");
+    });
+
+    it("deve renderizar o input na tela e verificar se o value é igual ao texto digitado pelo usuário", async() => {
+        const user = userEvent.setup();
+
+        render(
+            <Input 
+                placeholder="Digite um texto"
+            />
+        );
+
+        const inputElement = screen.getByPlaceholderText("Digite um texto");
+        await user.type(inputElement, "Texto digitado")
+
+        expect(inputElement).toBeInTheDocument();
+        expect(inputElement).toHaveValue("Texto digitado");
     });
 });
